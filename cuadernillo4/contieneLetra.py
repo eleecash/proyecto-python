3a8d6ac2afedfe2b975500a390b941cf010d06df
ele <edruidel@epsg.upv.es>
2025-02-12 11:41:36 +0100

    inicialcommit

---
+++ cuadernillo4/contieneLetra.py
@@ -0,0 +1,4 @@
+# Escribe una función que recibe el nombre de un fichero 
+# (cada línea puede tener varias palabras) y una letra, 
+# y que muestre por pantalla las palabras del fichero que 
+# contienen la letra.
--- cuadernillo4/ficheroPalabras.py
+++ cuadernillo4/ficheroPalabras.py
@@ -1,3 +1,18 @@
 # Escribe una función que reciba el nombre de un fichero que 
 # contiene palabras (cada línea tiene una palabra) y que devuelva la 
 # palabra que tiene una longitud máxima y su longitud.
+
+f = open("cuadernillo4/palabras.txt", mode="r", encoding="utf-8")
+
+def palabra_mas_larga(f):
+    palabras = f.readlines()
+    longitud = 0
+    palabra = ""
+    for p in palabras:
+        p = p.strip()
+        if len(p) > longitud:
+            longitud = len(p)
+            palabra = p
+    return palabra, longitud
+
+print(palabra_mas_larga(f))
